a3f6de57623245847fa10ed473e6383a761346f6
Bartzlo <ksb.bart@gmail.com>
2014-08-26 15:37:53 +0600

    1

--- src/server/Messager.java
+++ src/server/Messager.java
@@ -6,15 +6,15 @@ import static server.ServerChat.*;
 // Отправка сообщений клиентам
 public class Messager{
 
-    static DataOutputStream inpMes;
+    static DataOutputStream outMes;
 
     public static void sendMessage(int numClient, String message){
        if (socList.size() < 2) return;
         try{
             for (int i=0; i < socList.size(); i++){
                 if (i == numClient) continue; //если это отправитель, ему не посылаем это сообщение
-                inpMes = new DataOutputStream(socList.get(i).userSoc.getOutputStream());
-                inpMes.writeUTF(message);
+                outMes = new DataOutputStream(socList.get(i).userSoc.getOutputStream());
+                outMes.writeUTF(message);
             }
         }
         catch(Exception x){
